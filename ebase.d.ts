0358f48e95dedb84263040ca3c408039e743d0d5
David van Hussel <david@vanhussel.net>
2017-02-02 09:08:23 +0100

    executeFunction() and executeFunctionAsynchronously()

---
+++ ebase.d.ts
@@ -0,0 +1,18 @@
+		// Type definitions for Ebase Xi Client API v 5.2.1
+		// Project: Ebase Xi
+		// Definitions by: David van Hussel <https://www.haarlem.nl/>
+		
+		declare class $eb {
+		
+		    /**
+		     * executeFunction
+		     */
+		    
+		    static executeFunction(functionName: string, parameter?: any[], autorefresh?: boolean, fullsubmit?: boolean, failureCallBack?: any): any;
+		
+		    /**
+		     *  executeFunctionAsynchronously
+		     */
+		    static executeFunctionAsynchronously(successCallBack:any, failureCallBack:any, functionName:string, parameter:any[], autorefresh?:boolean, fullsubmit?:boolean): void;
+		
+		}
